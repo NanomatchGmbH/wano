95c6cf5fc342b5da51b4ea244d3eec2c69009b9f
Timo Strunk <Timo.Strunk@nanomatch.com>
2017-03-16 21:09:05 +0100

    Fixed kmc filepaths - bath

--- KMC_WaNo/exec_script.sh
+++ KMC_WaNo/exec_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source $EXTMOS/UBAH/config
-mpirun -np {{ wano["Simulation method"]["Number of processes"] }} $KMC_dir/KMC_PM simulation.ini {{ wano["Simulation method"]["Number of subgroups"] }} $KMC_inputs/ $KMC_outputs/
+mkdir kmc_output
+mpirun -np {{ wano["Simulation method"]["Number of processes"] }} $KMC_dir/KMC_PM simulation.ini {{ wano["Simulation method"]["Number of subgroups"] }} ./ ./kmc_output
