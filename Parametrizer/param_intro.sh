fb535b13215698b9c2018059a5909be6a076008f
Timo Strunk <Timo.Strunk@nanomatch.com>
2017-04-05 16:45:39 +0200

    First parameterizer wano

---
+++ Parametrizer/param_intro.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+source $EXTMOS/KIT/config
+
+WORKING_DIR=`pwd`
+
+export OMP_NUM_THREADS=$UC_PROCESSORS_PER_NODE
+
+if [ $OMP_NUM_THREADS -gt 1 ]
+then
+        #Turbomole config for multi cpu runs
+        export PARA_ARCH=SMP
+        export PARNODES=$OMP_NUM_THREADS
+        export PATH=$TURBODIR/bin/`sysname`:$PATH
+fi
+
+export MOLECULE="{{ wano["Molecule (Mol2)"] }}"
+INPUTBASE="${MOLECULE%.mol2}"
+export SHREDDER_INPUT="$INPUTBASE.cml"
+
+babel $MOLECULE $SHREDDER_INPUT
+babel $MOLECULE ${INPUTBASE}_2dsketch.png
+
+export DFTENGINE="{{ wano["Hardware Parameters"]["Engine"] }}"
+export DFT_MEMORY="{{ wano["Hardware Parameters"]["DFT Memory [MB]"] | int }}"
+
+export DO_GEOOPT="{{ wano["Geometrical Optimization"]["Optimize"] }}"
+export BASIS="{{ wano["Geometrical Optimization"]["Basis"] }}"
+export FUNCTIONAL="{{ wano["Geometrical Optimization"]["Functional"] }}"
+
+if [ "$DO_GEOOPT" == "True" ]
+then
+    python2 $SHREDDERBIN/exe_generate_settings.py
+    echo "python2 $SHREDDERBIN/geometrical_optimization.py $SHREDDER_INPUT"
+    python2 $SHREDDERBIN/geometrical_optimization.py $SHREDDER_INPUT
+    
+    babel opt_${INPUTBASE}.xyz $MOLECULE
+    babel opt_${INPUTBASE}.xyz $SHREDDER_INPUT    
+fi
+
+export BASIS="{{ wano["Partial Charge Settings"]["Basis"] }}"
+export FUNCTIONAL="{{ wano["Partial Charge Settings"]["Functional"] }}"
+
+rm -f settings.yml
+
+Deposit.py -calc-charges $MOLECULE $MOLECULE
+Deposit.py -generate_spf_from_mol2 $MOLECULE $INPUTBASE.spf
+Deposit.py -render-tachyon $MOLECULE
+
+packReport.py $MOLECULE
+nanoReporter.py --input=report.zip --type=pdf
