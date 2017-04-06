03dbd7da1dda9930ba20573f53ef548527c67e2a
Timo Strunk <Timo.Strunk@nanomatch.com>
2017-04-06 18:05:31 +0200

    DihedralParam race condition fix

--- DihedralParametrizer/dhparam_intro.sh
+++ DihedralParametrizer/dhparam_intro.sh
@@ -14,6 +14,26 @@ export FUNCTIONAL="{{ wano["Parameters"]["Functional"] }}"
 
 Deposit.py -prepare-dihedral-files "{{ wano["Deposit PDB"] }}" "{{ wano["Deposit SPF"] }}" "{{ wano["Parameters"]["Number of Steps"] | int }}"
 
-find . -maxdepth 1 -mindepth 1 -type f -name "*.xml.gz" | parallel -j $UC_PROCESSORS_PER_NODE 'Deposit.py -run-dihedral-file "{}" 1 > "{.}".out'
+rm -f *.sml *.cml
+
+for xmlfile in $(find . -maxdepth 1 -mindepth 1 -type f -name "*.xml.gz")
+do
+    mybase=${xmlfile%.xml.gz}
+    mkdir -p $mybase
+    mv $xmlfile $mybase
+done
+
+OLDPWD=$PWD
+
+
+find . -maxdepth 2 -mindepth 2 -type f -name "*.xml.gz" | parallel -j $UC_PROCESSORS_PER_NODE 'cd {//} && pwd && Deposit.py -run-dihedral-file {/} 1 > {/.}.out'
+
+
+cd $OLDPWD
+find . -name "*_snap.pdb" | xargs mv -t .
 
 Deposit.py -assemble-potential "{{ wano["Deposit SPF"] }}"
+
+rm molecule.spf
+mv molecule.pdb molecule_dh.pdb
+mv dihedral_forcefield.spf molecule_dh.spf
