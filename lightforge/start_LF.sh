afdde03dce30d1ade490b3b6ae09632d8ada2c68
Timo Strunk <Timo.Strunk@nanomatch.com>
2017-11-20 16:13:00 +0100

    Added lightforge wano

---
+++ lightforge/start_LF.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source $EXTMOS/KIT/config
+
+export OMP_NUM_THREADS=1
+
+if [ "$UC_TOTAL_PROCESSORS" -gt 1 ]
+then
+    $MPI_PATH/bin/mpirun -np $UC_TOTAL_PROCESSORS lightforge.py -s settings
+else
+    lightforge.py -s settings
+fi 
+zip -r outpout.zip range_expander/nearest_neighbour_distance_type1.png range_expander/nearest_neighbour_distance_type2.png material/energy_crosssection_*x.png material/transfer_integrals experiments/*.png experiments/current_density_* experiments/IQE* experiments/mobilities_* range_expander/*PBC.xyz
