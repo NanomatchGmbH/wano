c92e7dd661004773fe932733c9d44b4ba4097516
Franz Symalla <Franz.Symalla@nanomatch.com>
2018-02-15 21:19:24 +0100

    epsilon dist off by default. if run sucessfull create DriftDiffusion-in.yml

--- lightforge/start_LF.sh
+++ lightforge/start_LF.sh
@@ -5,9 +5,15 @@ export OMP_NUM_THREADS=1
 
 if [ "$UC_TOTAL_PROCESSORS" -gt 1 ]
 then
-    $MPI_PATH/bin/mpirun -np $UC_TOTAL_PROCESSORS lightforge.py -s settings
+    $MPI_PATH/bin/mpirun -np $UC_TOTAL_PROCESSORS -x PATH lightforge.py -s settings
 else
     lightforge.py -s settings
 fi 
 zip -r outpout.zip  material/*.png experiments/*.dat experiments/*.png experiments/*.txt
 
+count=`ls -1 experiments/*.png 2>/dev/null | wc -l`
+if [ $count != 0 ];then 
+	if [ ! -f DriftDiffusion-in.yml ]; then
+    	touch DriftDiffusion-in.yml
+	fi
+fi 
