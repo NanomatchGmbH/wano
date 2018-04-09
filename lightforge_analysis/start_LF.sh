3022366baea7eca5ed5b3f2dbb8239f03fcd7e8c
Franz Symalla <Franz.Symalla@nanomatch.com>
2018-04-09 16:01:50 +0200

    some change in analysis wano

--- lightforge_analysis/start_LF.sh
+++ lightforge_analysis/start_LF.sh
@@ -5,9 +5,10 @@ cp settings autorun_settings
 echo "analysis_autorun: True" >> autorun_settings
 mkdir experiments
 mkdir experiments/replay_0
+unzip replay.zip
+mv part*.npz experiments/replay_0/
 mkdir material
 cp material.npz material/material_0.npz
-cp part*.npz experiments/replay_0/
 echo "job_id: 0 settings_id: 0 mat_id: 0 field_direction: [1.0, 0.0, 0.0] field_strength: 0.03 initial_charges: [0, 0] Temperature: 300.0\n" > experiments/experiment_inventory_0.txt
 
 export OMP_NUM_THREADS=1
@@ -16,7 +17,7 @@ if [ "$UC_TOTAL_PROCESSORS" -gt 1 ]
 then
     $MPI_PATH/bin/mpirun --mca btl ^openib -x PATH -x PYTHONPATH -x SCRATCH -hostfile $HOSTFILE lightforge.py -s settings
 else
-    lightforge.py -s autorun_settings
+    $MPI_PATH/bin/mpirun -np 1 lightforge.py -s autorun_settings
 fi 
 #zip -r outpout.zip  material/*.png experiments/*.dat experiments/*.png experiments/*.txt
 
