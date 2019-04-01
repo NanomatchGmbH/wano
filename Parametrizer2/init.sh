279a3a1caa64b749225ea935650c75b1ce89dbd7
Timo Strunk <Timo.Strunk@nanomatch.com>
2019-04-01 15:09:40 +0200

    Removed QPDev environment from PArameterizer2, added addition of dihedral angles

--- Parametrizer2/init.sh
+++ Parametrizer2/init.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-### REMOVE FOR RELEASE, WORKAROUND FOR NMC DEV ENVIRONMENT ###
-export NANOMATCH=/home/nanodev_build/nanomatch/
-### REMOVE FOR RELEASE, WORKAROUND FOR NMC DEV ENVIRONMENT ###
-
 source $NANOMATCH/configs/quantumpatch.config
 
 /usr/bin/env python3 init_parametrizer.py
@@ -11,4 +7,15 @@ source $NANOMATCH/configs/quantumpatch.config
 echo "Running $NANOMATCH/QuantumPatch/MolecularTools/Parametrizer.py"
 $NANOMATCH/QuantumPatch/MolecularTools/Parametrizer.py
 
+
+DEPTOOLS=$NANOMATCH/deposit3/Tools
+SIMPY=$NANOMATCH/simona/python
+export PYTHONPATH=$SIMPY:$PYTHONPATH
+
+python3 $DEPTOOLS/SPFgeneratorV2.py output_molecule.mol2 temporary_molecule_with_dhs.spf
+python3 ./merge_dh.py molecule.spf temporary_molecule_with_dhs.spf > molecule_with_dh.spf
+
+rm temporary_molecule_with_dhs.spf
+mv molecule_with_dh.spf molecule.spf
 zip report.zip output_molecule.mol2 molecule.pdb molecule.spf
+
