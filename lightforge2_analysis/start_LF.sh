761d2333f2b8bd4033b830b3d4c993ecc0425210
Timo Strunk <Timo.Strunk@nanomatch.com>
2019-11-24 16:08:57 +0100

    Moved lightforge_analysis to versioned wanos

---
+++ lightforge2_analysis/start_LF.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+export NANOVER=V2
+source $NANOMATCH/$NANOVER/configs/lightforge.config
+cp settings autorun_settings
+echo "analysis_autorun: True" >> autorun_settings
+mkdir experiments
+mkdir experiments/replay_0
+unzip replay.zip
+unzip QP_inputs.zip
+mv part*.npz experiments/replay_0/
+mkdir material
+cp material.npz material/material_0.npz
+echo "job_id: 0 settings_id: 0 mat_id: 0 field_direction: [1.0, 0.0, 0.0] field_strength: 0.03 initial_charges: [0, 0] Temperature: 300.0\n" > experiments/experiment_inventory_0.txt
+
+export OMP_NUM_THREADS=1
+
+if [ "$UC_TOTAL_PROCESSORS" -gt 1 ]
+then
+    $MPI_PATH/bin/mpirun -genvall -machinefile $HOSTFILE lightforge.py -s autorun_settings
+else
+    $MPI_PATH/bin/mpirun -n 1 lightforge.py -s autorun_settings
+fi 
+#zip -r outpout.zip  material/*.png experiments/*.dat experiments/*.png experiments/*.txt
+
