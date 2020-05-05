3cfb6e84d611e225bab58590a7352f1bd2b87a82
Daniel <daniel.holub@nanomatch.com>
2020-05-05 16:01:24 +0200

    small change in the used memory for parametrizer3

--- Parametrizer3/init_parametrizer.py
+++ Parametrizer3/init_parametrizer.py
@@ -13,7 +13,7 @@ with open("rendered_wano.yml", "r") as ymlin:
 
 wano["Molecule Settings"]["Parameter File"] = 'default'
 wano["DFT Engine"]["Threads"] = environ["UC_TOTAL_PROCESSORS"]
-wano["DFT Engine"]["Memory (MB)"] = environ["UC_MEMORY_PER_NODE"]
+wano["DFT Engine"]["Memory (MB)"] = float(environ["UC_MEMORY_PER_NODE"]) * 0.85
 
 with open("parametrizer_settings.yml", "w") as ymlout:
     yaml.safe_dump(wano, ymlout)
