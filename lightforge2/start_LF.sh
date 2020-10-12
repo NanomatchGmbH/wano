607d92896ae7ebede54f98cf4310bee838cc08fc
Timo Strunk <Timo.Strunk@nanomatch.com>
2020-10-12 11:37:45 +0200

    Ported lightforge wano back to openmpi

--- lightforge2/start_LF.sh
+++ lightforge2/start_LF.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export NANOVER="V3"
+export NANOVER="V4"
 source $NANOMATCH/$NANOVER/configs/lightforge.config
 
 if [ -f lf_output.zip ]; then
@@ -11,6 +11,66 @@ fi
 
 export OMP_NUM_THREADS=1
 
+# Here we check, whether variables are set and add them to the mpirun exports. This is not required for mpirun with PBS/Torque, but required with everything else.
+# We could also specify only the required ones, but we do not know that a priori (i.e. whether Turbomole or Gaussian is to be used.
+# To avoid warnings, we therefore check first whether something is set and only then add it to the command.
+function varisset {
+        if [ -z ${!1+x} ]
+        then
+                echo "false"
+        else
+                echo "true"
+        fi
+}
+environmentvariables=("CGPATH"   \
+    "DALTONPATH"   \
+    "DEPOSITPATH"   \
+    "DEPTOOLS"   \
+    "DFTBPARAMETERS"   \
+    "DFTBPATH"   \
+    "DIHEDRAL_PARAMETRIZER_PATH"   \
+    "HOSTFILE"   \
+    "IBIPATH"   \
+    "KMCDEPOSITPATH"   \
+    "LD_LIBRARY_PATH"   \
+    "LFPATH"   \
+    "LOCAL"   \
+    "LOCALCONDA"   \
+    "MPI_PATH"   \
+    "NANOMATCH"   \
+    "NANOVER"   \
+    "NM_LICENSE_SERVER"   \
+    "OMP_NUM_THREADS"   \
+    "OPENMMPATH"   \
+    "OPENMPIPATH"    \
+    "PATH"   \
+    "PARNODES" \
+    "PYTHONPATH"   \
+    "SCRATCH"   \
+    "SHREDDERPATH"   \
+    "SIMONAPATH"   \
+    "SLURM_CPU_BIND"   \
+    "THREADFARMPATH"   \
+    "TURBODIR"   \
+    "UC_MEMORY_PER_NODE"   \
+    "UC_NODES"   \
+    "UC_PROCESSORS_PER_NODE"   \
+    "UC_TOTAL_PROCESSORS"   \
+    "UC_TOTAL_PROCESSORS"   \
+    "TURBODIR"   \
+    "TURBOMOLE_SYSNAME" \
+    "XTBPATH"   )
+
+
+ENVCOMMAND=""
+for var in "${environmentvariables[@]}"
+do
+  if [ "$(varisset ${var})" == "true" ]
+  then
+        ENVCOMMAND="$ENVCOMMAND -x $var"
+  fi
+done
+
 python reformat_settings.py settings
 if [ -f override_settings.yml ]
 then
@@ -19,7 +79,7 @@ fi
 
 if [ "$UC_TOTAL_PROCESSORS" -gt 1 ]
 then
-    $MPI_PATH/bin/mpirun -genvall -machinefile $HOSTFILE python -m mpi4py $LFPATH/lightforge.py -s settings
+    $OPENMPI_PATH/bin/mpirun --bind-to none $ENVCOMMAND --hostfile $HOSTFILE --mca vader,tcp python -m mpi4py $LFPATH/lightforge.py -s settings
 else
     lightforge.py -s settings
 fi 
