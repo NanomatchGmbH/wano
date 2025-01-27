0e9c492ec9594afd13fb86b705427b61fa4b25f8
Artem Fediai <artem.fediai@nanomatch.com>
2025-01-27 16:43:20 +0100

    add additional constraints for using half of the nodes threads

--- ESAnalysis/init_dos.py
+++ ESAnalysis/init_dos.py
@@ -115,7 +115,8 @@ def get_IPEA_settings(wano, cpu_per_node=1, tot_nodes = 1):
     eval_engine = cfg["DFTEngine"]["user"][f"{GW_engine} Evalstep"]
     eval_engine["functional"] = GW_func
     mem_scale = {"PySCF": 0.85, "Turbomole": 0.75}
-    if core_mode == "Number of Molecules of each Type":
+    if core_mode == "Number of Molecules of each Type" and mem_per_cpu*cpu_per_node > 500000 and tot_nodes == 1: 
+        # below happens _only_ on relatively thick node. Should work up to molecule size of SpiroOMeTAD
         # assumes two molecule types. Determining exact amount of moltype does not make sense: we does not know the min. memory required for GW of a given molecule.
         eval_engine["threads"] = cpu_per_node//2 - 1
     else:
